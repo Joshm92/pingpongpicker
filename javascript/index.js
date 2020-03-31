78d21db90e5935008af45779715e85cb5cd0e326
Josh <joshmarston@hotmail.co.uk>
2020-03-31 16:47:47 +0100

    Started JavaScript and adding functionality

---
+++ javascript/index.js
@@ -0,0 +1,56 @@
+((d) => {
+
+    let addButton = d.getElementById("addnamebutton"); //this is the add player name button
+
+    let input = d.getElementById("player") // this is the player name input field
+
+    let ul = d.getElementById("list") // this is the list where players will be displayed
+
+    // let players = [];
+
+    let players = Array.from(document.getElementsByClassName("player"));
+
+
+    addButton.addEventListener("click", () => {
+        let fragment = d.createDocumentFragment();
+
+        players.forEach((players) => {
+            let li = d.createElement("li");
+            
+            li.textContext = players;
+            fragment.append(li);
+            ul.prepend(li);
+        })
+   
+
+    ul.append(fragment);
+
+    players = [];
+
+    console.log(players);
+
+    })
+
+
+
+
+    //WORKING FUNCTION BELOW FOR ADD PLAYER BUTTON BUT IT'S NOT ADDING TO AN ARRAY JUST A LIST ITEM.
+
+//     addButton.addEventListener("click", () => {
+        
+//         let li = d.createElement('li');
+//         li.textContent = input.value;
+//         li.classList.add("list-group-item");
+//         player.value = "";
+//         player.focus();
+//         ul.prepend(li);
+
+//         console.log(li);
+//   });
+
+
+    //WORKING FUNCTION BELOW FOR GENERATE TEAM BUTTON BUT I DON'T KNOW HOW TO GET THE VALUES INTO THE ARRAY TO MAP OVER TO THEIR ELEMENTS.
+
+
+
+})(document);
